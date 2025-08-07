03cad3d4384c3972880353aaf4004869591ac976
MalusDomestica <a852741@hotmail.com.hk>
2025-08-08 07:00:06 +0800

    chore: add mongodb connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,15 @@
+import { config } from "./config";
 import app from "./app";
+import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+mongoose
+  .connect(config.mongoUri)
+  .then(() => {
+    console.log("Connected MongoDB:", mongoose.connection.name);
+    console.log("Host:", mongoose.connection.host);
+    console.log("Port:", mongoose.connection.port);
+    app.listen(config.port, () => {
+      console.log(`Server running at http://localhost:${config.port}`);
+    });
+  })
+  .catch((err) => console.error("DB connection error:", err));
