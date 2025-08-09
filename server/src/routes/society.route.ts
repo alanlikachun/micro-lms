5467a160e9f454fe8bf0dec23913f5a4958c32cc
MalusDomestica <a852741@hotmail.com.hk>
2025-08-09 15:00:41 +0800

    feat: add society crud request validator

--- server/src/routes/society.route.ts
+++ server/src/routes/society.route.ts
@@ -6,13 +6,20 @@ import {
   updateSociety,
   deleteSocieties,
 } from "../controllers/society.controller";
+import { validateResource } from "../middlewares/validateResource";
+import {
+  createSocietySchema,
+  deleteSocietiesSchema,
+  getSocietySchema,
+  updateSocietySchema,
+} from "../schemas/society.schema";
 
 const router = express.Router();
 
-router.post("/", createSociety);
+router.post("/", validateResource(createSocietySchema), createSociety);
 router.get("/", getSocieties);
-router.get("/:id", getSociety);
-router.patch("/:id", updateSociety);
-router.delete("/", deleteSocieties);
+router.get("/:id", validateResource(getSocietySchema), getSociety);
+router.patch("/:id", validateResource(updateSocietySchema), updateSociety);
+router.delete("/", validateResource(deleteSocietiesSchema), deleteSocieties);
 
 export default router;
---
+++ server/src/schemas/society.schema.ts
@@ -0,0 +1,38 @@
+import z from "zod";
+import { mongoObjectIdSchema } from "../utils/mongoObjectIdSchema";
+
+export const createSocietySchema = z.object({
+  body: z.object({
+    name: z.string().min(2),
+    managedBy: z.array(mongoObjectIdSchema).optional(),
+  }),
+});
+
+export const getSocietySchema = z.object({
+  params: z.object({
+    id: mongoObjectIdSchema,
+  }),
+});
+
+export const updateSocietySchema = z.object({
+  params: z.object({
+    id: mongoObjectIdSchema,
+  }),
+  body: z.object({
+    name: z.string().min(2).optional(),
+    managedBy: z.array(mongoObjectIdSchema).optional(),
+  }),
+});
+
+export const deleteSocietiesSchema = z.object({
+  body: z.object({
+    idList: z.array(mongoObjectIdSchema),
+  }),
+});
+
+export type CreateSocietyInput = z.infer<typeof createSocietySchema>;
+export type GetSocietyByIdInput = z.infer<typeof getSocietySchema>["params"];
+export type UpdateSocietyInput = z.infer<typeof updateSocietySchema>;
+export type DeleteSocietiesInput = z.infer<
+  typeof deleteSocietiesSchema
+>["body"];
