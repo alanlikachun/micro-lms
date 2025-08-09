8bf9ff0f0a3dc5e8b191f53e507a1324a4e9fcf7
MalusDomestica <a852741@hotmail.com.hk>
2025-08-10 07:17:07 +0800

    feat: apply auth middleware to create and delete user api

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import User, { Role } from "../models/user.model";
+import bcrypt from "bcrypt";
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, email, password, role } = req.body;
@@ -9,10 +10,12 @@ export const createUser = async (req: Request, res: Response) => {
     return res.status(409).json({ message: "User exists" });
   }
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const user = await User.create({
     name,
     email,
-    password: password,
+    password: hashedPassword,
     role: role || Role.STUDENT,
   });
 
