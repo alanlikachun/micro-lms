29f1cdcb3d139706d67fba5318551b29372cff0d
MalusDomestica <a852741@hotmail.com.hk>
2025-08-09 11:30:40 +0800

    feat: add user management page and integrate create and get user api

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HelloWorld from "../pages/HelloWorld.vue";
 import PageNotFound from "../pages/PageNotFound.vue";
+import UserManagement from "../pages/UserManagement.vue";
 
 const routes = [
   { path: "/", component: HelloWorld },
+  { path: "/user-management", component: UserManagement },
   { path: "/:pathMatch(.*)*", component: PageNotFound },
 ];
 
